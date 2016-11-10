9e8ab2d48d6e8bc656b09db7a38322fb275e3713
alchemistmatt <matthew.monroe@pnnl.gov>
2016-11-09 20:38:48 -0800

    Return an error code if there is a problem

--- MzidToTsvConverter/Program.cs
+++ MzidToTsvConverter/Program.cs
@@ -4,13 +4,13 @@ namespace MzidToTsvConverter
 {
     public static class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             var options = new ConverterOptions();
             if (!options.ProcessArgs(args))
             {
                 System.Threading.Thread.Sleep(1500);
-                return;
+                return -1;
             }
 
             options.OutputSetOptions();
@@ -21,11 +21,18 @@ namespace MzidToTsvConverter
                 converter.ConvertToTsv(options);
                 Console.WriteLine("Conversion finished!");
                 System.Threading.Thread.Sleep(700);
+                return 0;
             }
             catch (Exception e)
             {
                 Console.WriteLine("Conversion failed: " + e.Message);
+                Console.WriteLine(e.StackTrace);
+
                 System.Threading.Thread.Sleep(1500);
+                var errorCode = e.Message.GetHashCode();
+                if (errorCode == 0)
+                    return -1;
+                return errorCode;
             }
         }
     }
