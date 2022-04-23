e495d065ba72f7fbfe66c867c0232f9751c4edad
alchemistmatt <matthew.monroe@pnnl.gov>
2022-04-22 17:02:22 -0700

    Suppress warning and udpate program date

--- MzidToTsvConverter/ConverterOptions.cs
+++ MzidToTsvConverter/ConverterOptions.cs
@@ -26,7 +26,7 @@ namespace MzidToTsvConverter
         /// <summary>
         /// Program build date
         /// </summary>
-        public const string PROGRAM_DATE = "2022-04-18";
+        public const string PROGRAM_DATE = "2022-04-22";
 
         public ConverterOptions()
         {
