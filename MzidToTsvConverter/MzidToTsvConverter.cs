d4c2e6c0968ada7feafd15ab59ae77ee32ee6af3
alchemistmatt <matthew.monroe@pnnl.gov>
2022-04-22 17:05:28 -0700

    Convert to using declarations

--- MzidToTsvConverter/MzidToTsvConverter.cs
+++ MzidToTsvConverter/MzidToTsvConverter.cs
@@ -141,179 +141,181 @@ namespace MzidToTsvConverter
                     Delimiter = "\t"
                 };
 
-                using (var data = reader.ReadLowMem(mzidPath))
-                using (var writer = new StreamWriter(new FileStream(tsvFile.FullName, FileMode.Create, FileAccess.Write, FileShare.ReadWrite)))
-                using (var csv = new CsvWriter(writer, configuration))
+                using var data = reader.ReadLowMem(mzidPath);
+                using var writer = new StreamWriter(new FileStream(tsvFile.FullName, FileMode.Create, FileAccess.Write, FileShare.ReadWrite));
+                using var csv = new CsvWriter(writer, configuration);
+
+                csv.Context.RegisterClassMap(new PeptideMatchMap(options.NoExtendedFields, options.AddGeneId));
+
+                // SPECIAL CASE:
+                // Certain versions of MS-GF+ output incorrect mzid files - the peptides referenced in the peptide_ref attribute in
+                // SpectrumIdentificationItems was correct, but if there was a modification in the first 3 residues there was at
+                // least a 50% chance of the PeptideEvidenceRefs within the SpectrumIdentificationItem being incorrect. So, for
+                // those bad versions, use the peptide_ref rather than the PeptideEvidenceRefs to get the sequence.
+                var isBadMsGfMzid = false;
+
+                if (data.AnalysisSoftwareCvAccession.IndexOf("MS:1002048", StringComparison.OrdinalIgnoreCase) >= 0 &&
+                    !string.IsNullOrWhiteSpace(data.AnalysisSoftwareVersion))
                 {
-                    csv.Context.RegisterClassMap(new PeptideMatchMap(options.NoExtendedFields, options.AddGeneId));
-
-                    // SPECIAL CASE:
-                    // Certain versions of MS-GF+ output incorrect mzid files - the peptides referenced in the peptide_ref attribute in
-                    // SpectrumIdentificationItems was correct, but if there was a modification in the first 3 residues there was at
-                    // least a 50% chance of the PeptideEvidenceRefs within the SpectrumIdentificationItem being incorrect. So, for
-                    // those bad versions, use the peptide_ref rather than the PeptideEvidenceRefs to get the sequence.
-                    var isBadMsGfMzid = false;
-                    if (data.AnalysisSoftwareCvAccession.IndexOf("MS:1002048", StringComparison.OrdinalIgnoreCase) >= 0 &&
-                        !string.IsNullOrWhiteSpace(data.AnalysisSoftwareVersion))
+                    // bad versions: v10280 (introduced), v10282, v2016.01.20, v2016.01.21, v2016.01.29, v2016.02.12, v2016.05.25, v2016.0.13, v2016.06.13, v2016.06.14, v2016.06.15, v2016.06.29, v2016.07.26, v2016.08.31, v2016.09.07, v2016.09.22, v2016.09.23 (fixed with version v2016.10.10)
+                    var badVersions = new[]
                     {
-                        // bad versions: v10280 (introduced), v10282, v2016.01.20, v2016.01.21, v2016.01.29, v2016.02.12, v2016.05.25, v2016.0.13, v2016.06.13, v2016.06.14, v2016.06.15, v2016.06.29, v2016.07.26, v2016.08.31, v2016.09.07, v2016.09.22, v2016.09.23 (fixed with version v2016.10.10)
-                        var badVersions = new[]
+                        "v10280", "v10282",
+                        "v2016.01.20", "v2016.01.21", "v2016.01.29", "v2016.02.12", "v2016.05.25", "v2016.0.13",
+                        "v2016.06.13", "v2016.06.14", "v2016.06.15", "v2016.06.29", "v2016.07.26",
+                        "v2016.08.31", "v2016.09.07", "v2016.09.22", "v2016.09.23"
+                    };
+
+                    foreach (var version in badVersions)
+                    {
+                        if (data.AnalysisSoftwareVersion.Contains(version))
                         {
-                            "v10280", "v10282", "v2016.01.20", "v2016.01.21", "v2016.01.29", "v2016.02.12", "v2016.05.25", "v2016.0.13",
-                            "v2016.06.13",
-                            "v2016.06.14", "v2016.06.15", "v2016.06.29", "v2016.07.26", "v2016.08.31", "v2016.09.07", "v2016.09.22", "v2016.09.23"
-                        };
-                        foreach (var version in badVersions)
+                            isBadMsGfMzid = true;
+                        }
+                    }
+                }
+
+                if (isBadMsGfMzid)
+                {
+                    ConsoleMsgUtils.ShowWarning(
+                        "Warning: file \"{0}\" was created with a version of MS-GF+ that had some erroneous output in the mzid file." +
+                        " Using sequences from the peptide_ref attribute instead of the PeptideEvidenceRef element to try to bypass the issue.",
+                        mzidPath);
+                }
+
+                csv.WriteHeader<PeptideMatch>();
+                csv.NextRecord();
+
+                var lastScanNum = 0;
+
+                // Number of items in data.Identifications
+                // Incremented during the for each loop
+                var unfilteredCount = 0;
+
+                // Number of identifications that did not pass the score filters
+                var filteredOutCount = 0;
+
+                // List of matches in a single result. List is cleared before use.
+                // Only contains multiple when outputting all protein matches, and a result has multiple protein matches.
+                var matches = new List<PeptideMatch>(30);
+                foreach (var id in data.Identifications)
+                {
+                    if (options.SingleResultPerSpectrum && id.ScanNum == lastScanNum)
+                    {
+                        continue;
+                    }
+
+                    unfilteredCount++;
+
+                    lastScanNum = id.ScanNum;
+
+                    if (filterOnSpecEValue && id.SpecEv > options.MaxSpecEValue)
+                    {
+                        filteredOutCount++;
+                        continue;
+                    }
+
+                    if (filterOnEValue && id.EValue > options.MaxEValue)
+                    {
+                        filteredOutCount++;
+                        continue;
+                    }
+
+                    if (filterOnQValue && id.QValue > options.MaxQValue)
+                    {
+                        filteredOutCount++;
+                        continue;
+                    }
+
+                    // Clear out the list of matches.
+                    matches.Clear();
+                    var uniquePepProteinList = new HashSet<string>();
+
+                    // id.PepEvidence has one entry for each protein associated with this PSM
+                    IEnumerable<SimpleMZIdentMLReader.PeptideEvidence> pepEvEnum = id.PepEvidence;
+                    if (!options.ShowDecoy)
+                    {
+                        pepEvEnum = pepEvEnum.Where(x => !x.IsDecoy);
+                    }
+
+                    // maxMatchedProteins is '1' or 'int.MaxValue'
+                    foreach (var pepEv in pepEvEnum.Take(maxMatchedProteins))
+                    {
+                        var peptide = pepEv.SequenceWithNumericMods;
+
+                        // Produce correct output with bad MS-GF+ mzid
+                        if (isBadMsGfMzid)
                         {
-                            if (data.AnalysisSoftwareVersion.Contains(version))
+                            // Add the prefix and suffix residues for this protein
+                            // Do not use pepEv.SequenceWithNumericMods; it isn't necessarily correct for this spectrum
+                            peptide = pepEv.Pre + "." + id.Peptide.SequenceWithNumericMods + "." + pepEv.Post;
+                        }
+
+                        var protein = pepEv.DbSeq.Accession;
+
+                        if (!uniquePepProteinList.Add(peptide + protein))
+                        {
+                            // Don't process the check for the gene ID if it's not a unique match
+                            continue;
+                        }
+
+                        var geneId = string.Empty;
+                        if (options.AddGeneId && !pepEv.IsDecoy)
+                        {
+                            // Note that .ProteinDescription includes both the Protein Name and the Description
+                            var success = TryGetGeneId(options.GeneIdRegex, pepEv.DbSeq.ProteinDescription, out geneId);
+                            if (!success)
                             {
-                                isBadMsGfMzid = true;
+                                geneId = string.Empty;
                             }
                         }
+
+                        matches.Add(new PeptideMatch
+                        {
+                            SpecFile = data.SpectrumFile,
+                            Identification = id,
+                            Peptide = peptide,
+                            Protein = protein,
+                            GeneId = geneId,
+                        });
                     }
 
-                    if (isBadMsGfMzid)
+                    if (matches.Count == 0)
+                        continue;
+
+                    if (options.DelimitedProteinNames && matches.Count > 1)
                     {
-                        ConsoleMsgUtils.ShowWarning(
-                            "Warning: file \"{0}\" was created with a version of MS-GF+ that had some erroneous output in the mzid file." +
-                            " Using sequences from the peptide_ref attribute instead of the PeptideEvidenceRef element to try to bypass the issue.",
-                            mzidPath);
+                        CombineProteinNames(options, matches);
+
+                        // The first item in matches already lists all of the protein names; remove all remaining matches.
+                        matches.RemoveRange(1, matches.Count - 1);
                     }
 
-                    csv.WriteHeader<PeptideMatch>();
-                    csv.NextRecord();
-
-                    var lastScanNum = 0;
-
-                    // Number of items in data.Identifications
-                    // Incremented during the for each loop
-                    var unfilteredCount = 0;
-
-                    // Number of identifications that did not pass the score filters
-                    var filteredOutCount = 0;
-
-                    // List of matches in a single result. List is cleared before use.
-                    // Only contains multiple when outputting all protein matches, and a result has multiple protein matches.
-                    var matches = new List<PeptideMatch>(30);
-                    foreach (var id in data.Identifications)
+                    foreach (var item in matches)
                     {
-                        if (options.SingleResultPerSpectrum && id.ScanNum == lastScanNum)
-                        {
-                            continue;
-                        }
-
-                        unfilteredCount++;
-
-                        lastScanNum = id.ScanNum;
-
-                        if (filterOnSpecEValue && id.SpecEv > options.MaxSpecEValue)
-                        {
-                            filteredOutCount++;
-                            continue;
-                        }
-
-                        if (filterOnEValue && id.EValue > options.MaxEValue)
-                        {
-                            filteredOutCount++;
-                            continue;
-                        }
-
-                        if (filterOnQValue && id.QValue > options.MaxQValue)
-                        {
-                            filteredOutCount++;
-                            continue;
-                        }
-
-                        // Clear out the list of matches.
-                        matches.Clear();
-                        var uniquePepProteinList = new HashSet<string>();
-
-                        // id.PepEvidence has one entry for each protein associated with this PSM
-                        IEnumerable<SimpleMZIdentMLReader.PeptideEvidence> pepEvEnum = id.PepEvidence;
-                        if (!options.ShowDecoy)
-                        {
-                            pepEvEnum = pepEvEnum.Where(x => !x.IsDecoy);
-                        }
-
-                        // maxMatchedProteins is '1' or 'int.MaxValue'
-                        foreach (var pepEv in pepEvEnum.Take(maxMatchedProteins))
-                        {
-                            var peptide = pepEv.SequenceWithNumericMods;
-
-                            // Produce correct output with bad MS-GF+ mzid
-                            if (isBadMsGfMzid)
-                            {
-                                // Add the prefix and suffix residues for this protein
-                                // Do not use pepEv.SequenceWithNumericMods; it isn't necessarily correct for this spectrum
-                                peptide = pepEv.Pre + "." + id.Peptide.SequenceWithNumericMods + "." + pepEv.Post;
-                            }
-
-                            var protein = pepEv.DbSeq.Accession;
-
-                            if (!uniquePepProteinList.Add(peptide + protein))
-                            {
-                                // Don't process the check for the gene ID if it's not a unique match
-                                continue;
-                            }
-
-                            var geneId = string.Empty;
-                            if (options.AddGeneId && !pepEv.IsDecoy)
-                            {
-                                // Note that .ProteinDescription includes both the Protein Name and the Description
-                                var success = TryGetGeneId(options.GeneIdRegex, pepEv.DbSeq.ProteinDescription, out geneId);
-                                if (!success)
-                                {
-                                    geneId = string.Empty;
-                                }
-                            }
-
-                            matches.Add(new PeptideMatch
-                            {
-                                SpecFile = data.SpectrumFile,
-                                Identification = id,
-                                Peptide = peptide,
-                                Protein = protein,
-                                GeneId = geneId,
-                            });
-                        }
-
-                        if (matches.Count == 0)
-                            continue;
-
-                        if (options.DelimitedProteinNames && matches.Count > 1)
-                        {
-                            CombineProteinNames(options, matches);
-
-                            // The first item in matches already lists all of the protein names; remove all remaining matches.
-                            matches.RemoveRange(1, matches.Count - 1);
-                        }
-
-                        foreach (var item in matches)
-                        {
-                            csv.WriteRecord(item);
-                            csv.NextRecord();
-                        }
-
-                        writtenCount++;
+                        csv.WriteRecord(item);
+                        csv.NextRecord();
                     }
 
-                    if (unfilteredCount == 0)
+                    writtenCount++;
+                }
+
+                if (unfilteredCount == 0)
+                {
+                    ConsoleMsgUtils.ShowWarning("Warning: .mzid file does not have any results");
+                    Thread.Sleep(1500);
+                }
+                else if (writtenCount == 0)
+                {
+                    ConsoleMsgUtils.ShowWarning("Warning: none of the results passed the specified filter(s)");
+                    Thread.Sleep(1500);
+                }
+                else
+                {
+                    Console.WriteLine("Wrote {0:N0} results to {1}", writtenCount, PathUtils.CompactPathString(tsvFile.FullName, 70));
+                    if (filteredOutCount > 0)
                     {
-                        ConsoleMsgUtils.ShowWarning("Warning: .mzid file does not have any results");
-                        Thread.Sleep(1500);
-                    }
-                    else if (writtenCount == 0)
-                    {
-                        ConsoleMsgUtils.ShowWarning("Warning: none of the results passed the specified filter(s)");
-                        Thread.Sleep(1500);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Wrote {0:N0} results to {1}", writtenCount, PathUtils.CompactPathString(tsvFile.FullName, 70));
-                        if (filteredOutCount > 0)
-                        {
-                            Console.WriteLine("Filtered out {0:N0} results", filteredOutCount);
-                        }
+                        Console.WriteLine("Filtered out {0:N0} results", filteredOutCount);
                     }
                 }
 
