41e04b074fc0c495a5bb0566868f4aaaf714112e
alchemistmatt <matthew.monroe@pnnl.gov>
2022-04-27 11:26:26 -0700

    Add option to distribute files without pausing

--- MzidToTsvConverter/bin/Distribute_Files.bat
+++ MzidToTsvConverter/bin/Distribute_Files.bat
@@ -7,10 +7,10 @@ xcopy ..\..\Readme.md C:\DMS_Programs\MzidToTsvConverter /Y /D /F
 
 echo.
 echo Copying to \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MzidToTsvConverter
-pause
+if not "%1"=="NoPause" pause
 
 xcopy Release\net472\MzidToTsvConverter.exe \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MzidToTsvConverter /Y /D /F
 xcopy Release\net472\MzidToTsvConverter.pdb \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MzidToTsvConverter /Y /D /F
 xcopy Release\net472\*.dll \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MzidToTsvConverter /Y /D /F
 xcopy ..\..\Readme.md \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MzidToTsvConverter /Y /D /F
-pause
+if not "%1"=="NoPause" pause
