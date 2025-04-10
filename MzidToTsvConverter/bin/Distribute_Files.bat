778a5dbe0c3a8d46aac6626c0a16fd0a032f1427
alchemistmatt <matthew.monroe@pnnl.gov>
2025-04-09 17:38:35 -0700

    Align target directories in batch distribution file

--- MzidToTsvConverter/bin/Distribute_Files.bat
+++ MzidToTsvConverter/bin/Distribute_Files.bat
@@ -4,8 +4,8 @@ echo.
 
 xcopy Release\net48\MzidToTsvConverter.exe C:\DMS_Programs\MzidToTsvConverter /Y /D /F
 xcopy Release\net48\MzidToTsvConverter.pdb C:\DMS_Programs\MzidToTsvConverter /Y /D /F
-xcopy Release\net48\*.dll C:\DMS_Programs\MzidToTsvConverter /Y /D /F
-xcopy ..\..\Readme.md C:\DMS_Programs\MzidToTsvConverter /Y /D /F
+xcopy Release\net48\*.dll                  C:\DMS_Programs\MzidToTsvConverter /Y /D /F
+xcopy ..\..\Readme.md                      C:\DMS_Programs\MzidToTsvConverter /Y /D /F
 
 echo.
 echo Copying to \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MzidToTsvConverter
@@ -14,6 +14,6 @@ if not "%1"=="NoPause" pause
 
 xcopy Release\net48\MzidToTsvConverter.exe \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MzidToTsvConverter /Y /D /F
 xcopy Release\net48\MzidToTsvConverter.pdb \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MzidToTsvConverter /Y /D /F
-xcopy Release\net48\*.dll \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MzidToTsvConverter /Y /D /F
-xcopy ..\..\Readme.md \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MzidToTsvConverter /Y /D /F
+xcopy Release\net48\*.dll                  \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MzidToTsvConverter /Y /D /F
+xcopy ..\..\Readme.md                      \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MzidToTsvConverter /Y /D /F
 if not "%1"=="NoPause" pause
